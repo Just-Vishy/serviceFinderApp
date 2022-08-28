f48f22c2db4e62bb47aca59c6765b041ce8b1cfd
Vishnu <vblood.risencapricorn@gmail.com>
2022-08-28 23:06:06 +0200

    first commit

--- App.js
+++ App.js
@@ -1,13 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import Login from './src/Login';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './src/Home/Home';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+   return (
+    <NavigationContainer>
+      <Stack.Navigator>
+      <Stack.Screen name="Login" component={Login} options={{headerShown:false}} /> 
+      <Stack.Screen name="Home" component={Home} /> 
+      </Stack.Navigator>
+    </NavigationContainer>
+    
+    );
 }
 
 const styles = StyleSheet.create({
---
+++ src/Home/Data.js
@@ -0,0 +1,27 @@
+import cleaner from '../../assets/cleaner.png'
+import gardener from '../../assets/gardener.png'
+import hairdresser from '../../assets/hairdresser.png'
+const serviceData= [
+    {
+        name:"Cleaning Services",
+        category:"Cleaner",
+        numberCompany:"60",
+        image:cleaner,
+        bgColor:"#ea80fc"
+    }, {
+        name:"Garden Service",
+        category:"Gardener",
+        numberCompany:"30",
+        image:gardener,
+        bgColor:"#8c9eff"
+
+    }, {
+        name:"Hairdresser",
+        category:"Salon",
+        numberCompany:"50",
+        image:hairdresser,
+        bgColor:"#8c9eff"
+    }
+]
+
+export default serviceData;
